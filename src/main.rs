aef7f6eb68f937e59ffd8af41ff8507e25628e7b
Dino Maric <dino.onex@gmail.com>
2022-03-28 13:13:10 +0200

    Break structs into modules and different files/dirs.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
+use http::request::Request;
+use http::method::Method;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     let server = Server::new("127.0.0.1:3131".to_string());
     server.run();
-}
-
-struct Server {
-    address: String,
-}
-
-impl Server {
-    fn new(address: String) -> Self {
-        Self {
-            address
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.address);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: String,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    PATCH,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
 }
