2a101d9500a0506a4d20ef345c0a158206835bff
Dino Maric <dino.onex@gmail.com>
2022-04-01 11:59:02 +0200

    Receive http requests

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     address: String,
 }
@@ -11,5 +14,22 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.address);
+        let listener = TcpListener::bind(&self.address).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut socket, _address)) => {
+                    let mut buffer = [0; 1024];
+                    match socket.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => println!("Failed to read from connection {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to establish the coonection {}", e),
+            }
+
+        }
     }
 }
