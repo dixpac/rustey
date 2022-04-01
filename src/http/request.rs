3fe6e37832bae5a6cffd6fdc3a9005bb9413cfa6
Dino Maric <dino.onex@gmail.com>
2022-04-01 18:56:35 +0200

    Extract query string.

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,5 @@
 use std::str;
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::error::Error;
 use std::str::Utf8Error;
 use std::convert::From;
@@ -16,11 +16,33 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        let result = str::from_utf8(value)?;
+        let request = str::from_utf8(value)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1 " {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
 
         unimplemented!()
     }
 }
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (index, character) in request.chars().enumerate() {
+        if character == ' ' || character == '\r' {
+            return Some((&request[..index], &request[index + 1..]));
+        }
+    }
+
+    None
+}
+
 
 pub enum ParseError {
     InvalidRequest,
@@ -43,6 +65,12 @@ impl ParseError {
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
